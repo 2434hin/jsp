d097db328bf56d594d9fbab53830c24f9d4311a1
2434hin <2434hin@gmail.com>
2019-08-12 14:12:27 +0900

    1. server side cookie 기능 추가
    2. dao 생성 로직 수정

--- src/main/java/kr/or/ddit/login/web/LoginController.java
+++ src/main/java/kr/or/ddit/login/web/LoginController.java
@@ -23,6 +23,13 @@ public class LoginController extends HttpServlet {
    private static final long serialVersionUID = 1L;
    
    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+   
+   private IUserDao userDao;
+   
+   @Override
+	public void init() throws ServletException {
+	   userDao = new UserDao();
+	}
        
    /**
     * 
@@ -41,10 +48,12 @@ public class LoginController extends HttpServlet {
 	   
 	  // 웹브라우저가 보낸 cookie확인
 	  Cookie[] cookies = request.getCookies();
-	  for(Cookie cookie : cookies) {
-		  
-		  logger.debug("cookie name : {}, cookie value : {}", cookie.getName(), cookie.getValue());
-
+	  if(cookies != null) {
+		  for(Cookie cookie : cookies) {
+			  
+			  logger.debug("cookie name : {}, cookie value : {}", cookie.getName(), cookie.getValue());
+	
+		  }
 	  }
 	   
 	  // 응답을 생성할 때 웹브라우저에게 쿠키를 저장할 것을 지시
@@ -75,17 +84,17 @@ public class LoginController extends HttpServlet {
 	   String userId = request.getParameter("userId");
 	   String pass = request.getParameter("pass");
 	   
+	   String rememberMe = request.getParameter("rememberMe");
+	   
+	   // rememberMe 파라미터가 존재할 경우 userId를 cookie로 생성
+	   manageUserIdCookie(response, userId, rememberMe);
+	   
 	   logger.debug("userId : {}", userId);
 	   logger.debug("password : {}", pass);
 	   
 	   // 사용자가 입력한 계정정보와 db에 있는 값이랑 비교
-//	   UserVO userVo = new UserVO();
-//	   userVo.setUserNm("브라운");
-//	   userVo.setUserId("brown");
-//	   userVo.setPass("brown1234");
 	   
 	   //db에서 조회해온 사용자 정보
-	   IUserDao userDao = new UserDao();
 	   User user = userDao.getUser(userId);
 	   
 	   // 사용자가 입력한 파라미터 정보와 db에서 조회해온 값이 동일 할 경우 --> webapp/main.jsp
@@ -110,5 +119,17 @@ public class LoginController extends HttpServlet {
 		   doGet(request, response);
 	   }
    }
+
+	private void manageUserIdCookie(HttpServletResponse response, String userId, String rememberMe) {
+		Cookie cookie = new Cookie("userId", userId);
+		   
+		   if(rememberMe != null) {
+			   cookie.setMaxAge(60*60*24*30); // 30일
+		   }else {
+			   cookie.setMaxAge(0); // 삭제 -> :) 기간이 만료
+		   }
+		   
+		   response.addCookie(cookie);
+	}
 
 }
